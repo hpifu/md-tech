ae827df027c1de9c4c2013bab5d5cd1a151ae372
hatlonely <hatlonely@gmail.com>
2019-10-12 04:42:18 +0800

    update

--- scripts/mysql.py
+++ scripts/mysql.py
@@ -7,7 +7,7 @@ import json
 
 config = {
     "mysql": {
-        "host": "mysql",
+        "host": "127.0.0.1",
         "port": 3306,
         "user": "hatlonely",
         "password": "keaiduo1",
